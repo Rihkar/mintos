d672c6ed5155a6ebeb8d3f6942451c4d26bd794b
Knazat <rihk.karlauskis@gmail.com>
2022-05-19 18:54:10 +0300

    patirits kods

--- src/Components/Currencies.tsx
+++ src/Components/Currencies.tsx
@@ -1,24 +1,25 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable max-len */
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import CurrencyList, { CurrencyType } from '../Data/CurrencyList';
 
 const Currencies = () => {
   const [currencyArray, setCurrencyArray] = useState<CurrencyType[]>(CurrencyList);
 
-  const toggleSelected = (curr:CurrencyType) => {
-    setCurrencyArray(currencyArray.map((c) => {
-      if (c.id === curr.id) {
-        return { ...c, isSelected: !c.isSelected };
+  const toggleSelected = (currency:CurrencyType) => {
+    setCurrencyArray(currencyArray.map((item) => {
+      if (item.id === currency.id) {
+        return { ...item, isSelected: !item.isSelected };
       }
-      return c;
+      return item;
     }));
   };
   return (
     <div className="container">
       <div className="container__currency">
         {currencyArray.filter((el) => el.isSelected && el.currency).map((curr) => (
-          <div style={{ width: '30%' }} className="selected-currency" key={Math.random()}>
-            <span style={{ color: 'black', backgroundColor: '#d6d0d0' }}>{curr.currency.toLowerCase()}</span>
+          <div className="selected-currency" key={Math.random()}>
+            <span>{curr.currency.toLowerCase()}</span>
             <button
               className="button-remove"
               onClick={() => {
@@ -35,15 +36,13 @@ const Currencies = () => {
       <div className="container__currency padding-top">
         {currencyArray.map((curr) => (
           <button
-            style={curr.isSelected ? { backgroundColor: 'white' } : { backgroundColor: '#d6d0d0' }}
-            className="big-button-add"
+            className={`big-button-add   ${curr.isSelected && 'selected-button'} `}
             key={Math.random()}
             onClick={() => {
               toggleSelected(curr);
             }}
           >
             <button
-              style={curr.isSelected ? { backgroundColor: 'white' } : { backgroundColor: '#d6d0d0' }}
               className="small-button-add"
             >
               {curr.isSelected && 'X'}
